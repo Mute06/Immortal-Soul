7718909bdea0edf82e11d895c67cc88f97d3aef2
Mute06 <umutkanibir06@gmail.com>
2021-08-28 21:42:34 +0300

    LevelLoader and start scene finished

--- Zombie/Assets/Scripts/LevelLoader.cs
+++ Zombie/Assets/Scripts/LevelLoader.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static LevelLoader instance;
+    public Animator transition;
+    public float transitionTime = 1f;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadNextScene()
     {
-        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public void LoadScene(int levelIndex)
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
     }
 }
