8733135603de9fb64b6735df0466694ed90f6aa7
Mute06 <umutkanibir06@gmail.com>
2021-08-29 09:51:13 +0300

    Sounds and game end

--- Zombie/Assets/Scripts/Trigger.cs
+++ Zombie/Assets/Scripts/Trigger.cs
@@ -1,17 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
 {
+    public UnityEvent OnEnter; 
     [SerializeField] public GameObject objectToEnable;
+    public bool isDelayed;
+    public float delayTime;
     bool didWorked;
     private void OnTriggerEnter(Collider other)
     {
         if (!didWorked && other.CompareTag("Player") )
         {
+            
             objectToEnable.SetActive(true);
             didWorked = true;
+            if (isDelayed)
+            {
+                if (OnEnter != null)
+                {
+                    StartCoroutine(Delayed());
+                }
+            }
+            else
+            {
+                if (OnEnter != null)
+                {
+                    OnEnter.Invoke();
+                }
+            }
+
         }
     }
+    private IEnumerator Delayed()
+    {
+        yield return new WaitForSeconds(delayTime);
+        OnEnter.Invoke();
+    }
+
 }
